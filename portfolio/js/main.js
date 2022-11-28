7db8fdf27ede3f08bba9a1cff06f7bd603267278
LucasViniciusPgo <88382871+LucasViniciusPgo@users.noreply.github.com>
2022-11-27 21:03:01 -0300

    Delete portfolio directory

--- portfolio/js/main.js
+++
@@ -1,9 +0,0 @@
-function Enviar() {
- 
-    var nome = document.getElementById("nomeid");
- 
-    if (nome.value != "") {
-        alert('Prezado(a), ' + nome.value + ' os seus dados foram enviados com êxito');
-    }
- 
-}
